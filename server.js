3d14711d7b3b180f935235b35ab719c54d33a2c2
PrincipitoPJBB <pabalber@gmail.com>
2026-02-11 19:53:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -69,4 +69,9 @@ app.get("/", (req, res) => {
   res.send("Principito IA funcionando");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
