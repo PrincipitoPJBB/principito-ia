9b59585b8ce2fd46066d750af742e9df6566423f
PrincipitoPJBB <pabalber@gmail.com>
2026-02-12 10:11:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
 const SYSTEM_PROMPT = `
 Eres El Principito durante su viaje entre planetas.
